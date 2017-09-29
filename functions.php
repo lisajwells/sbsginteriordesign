8e949d15e483d6b3894eb833f0deb441b080ca1e
Lisa Wells <lisawells999@gmail.com>
2017-09-29 12:48:50 -0400

    comment unused enqueue

--- functions.php
+++ functions.php
@@ -1,22 +1,16 @@
 <?php
 
-function sbsg_enqueue_scripts() {
+// function sbsg_enqueue_scripts() {
     // wp_enqueue_script( 'add-sbsg-js', get_stylesheet_directory_uri() . '/js/sbsg.js', array( 'jquery' ), '1.0.0', true );
-}
-add_action( 'wp_enqueue_scripts', 'sbsg_enqueue_scripts', 200 );
-
-// https://premium.wpmudev.org/blog/adding-jquery-scripts-wordpress/
-// function my_theme_scripts() {
-    // wp_enqueue_script( 'my-great-script', get_template_directory_uri() . '/js/sbsg.js', array( 'jquery' ), '1.0.0', true );
 // }
-// add_action( 'wp_enqueue_scripts', 'my_theme_scripts' );
+// add_action( 'wp_enqueue_scripts', 'sbsg_enqueue_scripts', 200 );
 
 function metaslider_nivo_js($javascript, $slider_id) {
     $javascript .= "(function($) {
 $('.metaslider a').click(function() {
     $('html, body').animate({
         scrollTop: $('#metaslider_container_31').offset().top
-    }, 2000);
+    }, 1000);
 });
 })( jQuery );
 ";
