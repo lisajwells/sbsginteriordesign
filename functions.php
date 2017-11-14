bb1376ae94f229f25613ce0d0ba59124502ba856
Lisa Wells <lisawells999@gmail.com>
2017-11-14 13:29:03 -0500

    no autop for before-after page; adjust beaver for before-after page

--- functions.php
+++ functions.php
@@ -22,13 +22,12 @@ add_filter('metaslider_nivo_slider_javascript', 'metaslider_nivo_js', 10, 2);
 
 /* remove autop from white caption pages */
 function sbsg_wpautop_filter_control( $content ){
-    if ( is_front_page() || is_page( array( 'projects', 'contact', 'about' ) ) ){
+    if ( is_front_page() || is_page( array( 'projects', 'contact', 'about', 'before-after' ) ) ){
         return $content;
     } else {
         return wpautop($content);
     }
 }
-
 remove_filter( 'the_content', 'wpautop' );
 add_filter( 'the_content', 'sbsg_wpautop_filter_control' );
 
