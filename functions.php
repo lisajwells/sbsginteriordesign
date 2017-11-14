6fe2ada81eaa2fcdddf0d634ac3a4e8ad04cf204
Lisa Wells <lisawells999@gmail.com>
2017-11-14 17:47:55 -0500

    no autop for services page

--- functions.php
+++ functions.php
@@ -22,7 +22,7 @@ add_filter('metaslider_nivo_slider_javascript', 'metaslider_nivo_js', 10, 2);
 
 /* remove autop from white caption pages */
 function sbsg_wpautop_filter_control( $content ){
-    if ( is_front_page() || is_page( array( 'projects', 'contact', 'about', 'before-after' ) ) ){
+    if ( is_front_page() || is_page( array( 'projects', 'contact', 'about', 'services', 'before-after' ) ) ){
         return $content;
     } else {
         return wpautop($content);
